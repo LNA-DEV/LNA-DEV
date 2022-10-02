ab5f92dd0b42e42b1de8da04d6a6b533d4eb4e37
Lukas Nagel <lukas@lna-dev.net>
2022-10-02 22:08:35 +0200

    Updated Paths

--- GitHubStats/generate_images.py
+++ GitHubStats/generate_images.py
@@ -45,7 +45,7 @@ async def generate_overview(s: Stats) -> None:
     output = re.sub("{{ repos }}", f"{len(await s.repos):,}", output)
 
     generate_output_folder()
-    with open("generated/overview.svg", "w") as f:
+    with open("GitHubStats/generated/overview.svg", "w") as f:
         f.write(output)
 
 
@@ -54,7 +54,7 @@ async def generate_languages(s: Stats) -> None:
     Generate an SVG badge with summary languages used
     :param s: Represents user's GitHub statistics
     """
-    with open("templates/languages.svg", "r") as f:
+    with open("GitHubStats/templates/languages.svg", "r") as f:
         output = f.read()
 
     progress = ""
@@ -86,7 +86,7 @@ fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg>
     output = re.sub(r"{{ lang_list }}", lang_list, output)
 
     generate_output_folder()
-    with open("generated/languages.svg", "w") as f:
+    with open("GitHubStats/generated/languages.svg", "w") as f:
         f.write(output)
 
 
